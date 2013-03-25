c462381b252864e0dfc96fe32066a0fc46071d98
Kevin Gaddy <kevin.gaddy@gmail.com>
2013-03-25 13:02:40 -0500

    init
    Signed-off-by: Kevin Gaddy <kevin.gaddy@gmail.com>

---
+++ HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by kgaddy on 3/25/13.
+//  Copyright (c) 2013 com.kgaddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by kgaddy on 3/25/13.
+//  Copyright (c) 2013 com.kgaddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnoAppDelegate class]));
+    }
+}
